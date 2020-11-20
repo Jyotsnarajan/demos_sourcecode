3c846bc4f05654fa9e2568ce398c7824e1366177
Jyotsnarajan <jyotsnarajan.815@gmail.com>
2020-11-20 15:21:14 +0530

    removed brackets

--- l_bm3d/main.cpp
+++ l_bm3d/main.cpp
@@ -112,11 +112,10 @@ int main(int argc, char **argv)
 	//! Add noise
 	cout << endl << "Add noise [sigma = " << fSigma << "] ...";
 			
-	if (addnoise == 1){
-		add_noise(img, img_noisy, fSigma); }
-	
-	else {
-		img_noisy = img; }
+	if (addnoise == 1)
+		add_noise(img, img_noisy, fSigma); 	
+	else 
+		img_noisy = img; 
 		
     cout << "done." << endl;
 
